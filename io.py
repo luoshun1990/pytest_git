e64a808c212f919898f26bfb7107ad40b3b310fc
luoshun <623284003@qq.com>
2017-11-08 10:58:45 +0800

    change io.py

--- io.py
+++ io.py
@@ -6,6 +6,6 @@ with open('./test.txt','r') as f:
 		for line in f.readlines():
 			print(line.strip())
 
-#with open('./test.jpg','rb') as f1:
-#	print(f1.read())
+with open('./test.jpg','rb') as f1:
+	print(f1.read())
 
